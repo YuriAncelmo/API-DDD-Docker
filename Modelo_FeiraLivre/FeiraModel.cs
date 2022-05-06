4a13bbeedbe7bff576628bbcfe56ae84abbe2bee
yuria <yuriancelmo@hotmail.com>
2022-05-05 23:58:14 -0300

    Ajustando rotina de ingestão, modelo, e amanhã é possível já fazer a ingestão em um banco de dados.

---
+++ Modelo_FeiraLivre/FeiraModel.cs
@@ -0,0 +1,44 @@
+﻿namespace Modelo_FeiraLivre
+{
+    public class FeiraModel
+    {
+        public string ID;
+        public string LONG; 
+        public string LAT;
+        public string SETCENS; 
+        public string AREAP; 
+        public string CODDIST; 
+        public string DISTRITO; 
+        public string CODSUBPREF; 
+        public string SUBPREFE; 
+        public string REGIAO5; 
+        public string REGIAO8; 
+        public string NOME_FEIRA; 
+        public string REGISTRO;
+        public string LOGRADOURO; 
+        public string NUMERO; 
+        public string BAIRRO;
+        public string REFERENCIA;
+
+        public FeiraModel(string iD, string lONG, string lAT, string sETCENS, string aREAP, string cODDIST, string dISTRITO, string cODSUBPREF, string sUBPREFE, string rEGIAO5, string rEGIAO8, string nOME_FEIRA, string rEGISTRO, string lOGRADOURO, string nUMERO, string bAIRRO, string rEFERENCIA)
+        {
+            ID = iD ?? throw new ArgumentNullException(nameof(iD));
+            LONG = lONG ?? throw new ArgumentNullException(nameof(lONG));
+            LAT = lAT ?? throw new ArgumentNullException(nameof(lAT));
+            SETCENS = sETCENS ?? throw new ArgumentNullException(nameof(sETCENS));
+            AREAP = aREAP ?? throw new ArgumentNullException(nameof(aREAP));
+            CODDIST = cODDIST ?? throw new ArgumentNullException(nameof(cODDIST));
+            DISTRITO = dISTRITO ?? throw new ArgumentNullException(nameof(dISTRITO));
+            CODSUBPREF = cODSUBPREF ?? throw new ArgumentNullException(nameof(cODSUBPREF));
+            SUBPREFE = sUBPREFE ?? throw new ArgumentNullException(nameof(sUBPREFE));
+            REGIAO5 = rEGIAO5 ?? throw new ArgumentNullException(nameof(rEGIAO5));
+            REGIAO8 = rEGIAO8 ?? throw new ArgumentNullException(nameof(rEGIAO8));
+            NOME_FEIRA = nOME_FEIRA ?? throw new ArgumentNullException(nameof(nOME_FEIRA));
+            REGISTRO = rEGISTRO ?? throw new ArgumentNullException(nameof(rEGISTRO));
+            LOGRADOURO = lOGRADOURO ?? throw new ArgumentNullException(nameof(lOGRADOURO));
+            NUMERO = nUMERO ?? throw new ArgumentNullException(nameof(nUMERO));
+            BAIRRO = bAIRRO ?? throw new ArgumentNullException(nameof(bAIRRO));
+            REFERENCIA = rEFERENCIA ?? throw new ArgumentNullException(nameof(rEFERENCIA));
+        }
+    }
+}
--- Utilitario_FeirasLivresSP/Program.cs
+++ Utilitario_FeirasLivresSP/Program.cs
@@ -1,31 +1,35 @@
 ﻿using Microsoft.VisualBasic.FileIO;
+using Modelo_FeiraLivre;
 using System;
 using System.IO;
 using System.Threading;
-
 namespace Utilitario_FeirasLivresSP
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string BancoDeDados = "SQL Server";
-            EscrevaComEfeito("Olá! Esta é a documentação iterativa para o teste de software da Unico!");
+            string BancoDeDados = "MySQL";
 
-            EscrevaComEfeito("Podemos começar, fazendo a importação dos dados das feiras de São Paulo.");
 
-            EscrevaComEfeito("A Unico especificou que o arquivo utilizado deve ser o DEINFO_AB_FEIRASLIVRES_2014.csv.");
+            ImprimirCabecalho();
 
-            EscrevaComEfeito("Você pode continuar com o arquivo especificado ou selecionar um novo, o que quer fazer?");
+            RotinaIngestaoArquivo(BancoDeDados);
 
-            Console.WriteLine("1) Selecionar o arquivo DEINFO_AB_FEIRASLIVRES_2014.csv.");
-            Console.WriteLine("2) Selecionar outro arquivo.");
-            string opcao = Console.ReadLine();
+            Console.Read();
+        }
+
+        private static void RotinaIngestaoArquivo(string BancoDeDados)
+        {
             string localExecucaoAssembly = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
             string estruturaPastas = @"\FEIRAS_LIVRES\CSV\DEINFO_DADOS_AB_FEIRASLIVRES\";
             string nomeArquivo = "DEINFO_AB_FEIRASLIVRES_2014.csv";
 
-            if (opcao == "1")
+            ImprimirCabecalhoArquivo();
+
+            int opcao = LerOpcao();
+
+            if (opcao == 1)
             {
                 EscrevaComEfeito("Ok. Então não vamos inovar por enquanto.");
 
@@ -39,10 +43,16 @@ namespace Utilitario_FeirasLivresSP
 
                 EscrevaComEfeito("O arquivo está no caminho correto.");
 
-                EscrevaComEfeito(string.Format("Enquanto você lê essa mensagem, eu já estou importando os dados para um banco de dados relacional, chamado {0}.", BancoDeDados));
+                EscrevaComEfeito("Enquanto você lê essa mensagem, eu já estou importando os dados a memória.");
                 LerCSV(localExecucaoAssembly + estruturaPastas + nomeArquivo);
+                EscrevaComEfeito("Para fazer a configuração deste banco de dados, realizei o download em https://dev.mysql.com/downloads/file/?id=511553");
+                EscrevaComEfeito("Baixe a versão para desenvolvedor, padrão.");
+                EscrevaComEfeito("Vai demorar um pouquinho pra instalar... vá tomar um café enquanto espera, depois volte aqui (: (Não esqueça de dar um enter quando estiver tudo OK.)");
+                Console.Read();
+                EscrevaComEfeito("Estou utilizando para fazer acesso ao banco de dados , a biblioteca MySql.Data.MySqlClient .Net Core Class Library, versão 8.0.29");
 
-                //Combine that with System.IO.Path.GetDirectoryName if all you want is
+                Console.WriteLine("Deixa eu tentar conectar com o banco de dados, me dá um minuto.");
+
 
             }
             else
@@ -50,19 +60,56 @@ namespace Utilitario_FeirasLivresSP
                 EscrevaComEfeito("Então preciso que você escolha um arquivo, dentre os que estão na estrutura de pastas: " + localExecucaoAssembly + estruturaPastas);
                 EscrevaComEfeito("Que são os listados a seguir:");
                 string[] arquivos = Directory.GetFiles(localExecucaoAssembly + estruturaPastas);
-                while(arquivos.Length == 0 )
+                while (arquivos.Length == 0)
                 {
-                    EscrevaComEfeito(string.Format("Ué, parece que não tem nenhum arquivo na pasta... Você colocou em {0}? Depois de colocar, aperte ENTER",localExecucaoAssembly + estruturaPastas ));
+                    EscrevaComEfeito(string.Format("Ué, parece que não tem nenhum arquivo na pasta... Você colocou em {0}? Depois de colocar, aperte ENTER", localExecucaoAssembly + estruturaPastas));
                     Console.Read();
                 }
                 for (int i = 0; i < arquivos.Length; i++)
                 {
                     Console.WriteLine(string.Format("{0}) {1}", i, arquivos[i]));
                 }
-                opcao = Console.ReadLine();
+                opcao = LerOpcao();
+                LerCSV(arquivos[opcao]);
                 //Enquanto não for uma opção valida , não deixa seguir;
             }
-            Console.Read();
+        }
+
+        private static void ImprimirCabecalhoArquivo()
+        {
+            EscrevaComEfeito("Podemos começar, fazendo a importação dos dados das feiras de São Paulo.");
+
+            EscrevaComEfeito("A Unico especificou que o arquivo utilizado deve ser o DEINFO_AB_FEIRASLIVRES_2014.csv.");
+
+            EscrevaComEfeito("Você pode continuar com o arquivo especificado ou selecionar um novo, o que quer fazer?");
+
+            EscreverOpcao(1, "Selecionar o arquivo DEINFO_AB_FEIRASLIVRES_2014.csv.");
+
+            EscreverOpcao(2, "Selecionar outro arquivo.");
+        }
+
+        public static void ImprimirCabecalho()
+        {
+            EscrevaComEfeito("Olá! Esta é a documentação iterativa para o teste de software da Unico!");
+        }
+        private static int LerOpcao()
+        {
+            int opcao = -1;
+            while (opcao != -1)
+                try
+                {
+                    opcao = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Digite uma opção válida");
+                }
+            return opcao;
+        }
+
+        private static void EscreverOpcao(int numero, string descricao)
+        {
+            Console.WriteLine(string.Format("{0}) {1}", numero, descricao));
         }
 
         private static void LerCSV(string file)
@@ -71,15 +118,14 @@ namespace Utilitario_FeirasLivresSP
             {
                 parser.TextFieldType = FieldType.Delimited;
                 parser.SetDelimiters(",");
-                while (!parser.EndOfData)
+                while (!parser.EndOfData)//O(n)
                 {
                     //Process row
                     string[] fields = parser.ReadFields();
-                    foreach (string field in fields)
-                    {
-                        //TODO: Process field
-                        Console.Write(field);
-                    }
+                    FeiraModel model = new FeiraModel(fields[0], fields[1], fields[2],
+                        fields[3], fields[4], fields[5], fields[6], fields[7], fields[8],
+                        fields[9], fields[10], fields[11], fields[12], fields[13], fields[14],
+                        fields[15], fields[16]);
                 }
             }
         }
@@ -89,12 +135,12 @@ namespace Utilitario_FeirasLivresSP
             for (int i = 0; i < s.Length; i++)
             {
                 Console.Write(s[i]);
-#if !DEBUG
+#if DEBUG
                 Thread.Sleep(30);
 #endif
             }
             Console.WriteLine("\n");
-#if !DEBUG
+#if DEBUG
             Thread.Sleep(500);
 #endif
         }
